9fcb2e054423075e844d03edfed7b04300770a06
Yan <pelpvsp@gmail.com>
2023-06-10 17:25:05 +0300

    задача2

---
+++ sem1 z2/Program.cs
@@ -0,0 +1,7 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int Max = A;
+if(B>Max) Max = B;
+if(C>Max) Max = C;
+Console.WriteLine(Max);
